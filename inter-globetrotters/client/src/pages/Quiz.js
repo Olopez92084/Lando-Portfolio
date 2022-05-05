bf813990f7b4fae8d812da864017d58b6fdabe81
Orlando Lopez <o.lopez92084@gmail.com>
2022-05-04 23:25:27 -0400

    Add files via upload

---
+++ inter-globetrotters/client/src/pages/Quiz.js
@@ -0,0 +1,147 @@
+import React, { useState, useEffect } from "react";
+// import { useMutation } from "@apollo/client";
+import { Redirect } from "react-router-dom";
+// import { useQuery } from '@apollo/client';
+// import { RANDOM_PRODUCT } from "../utils/queries";
+import Jumbotron from '../components/Jumbotron';
+// import Auth from "../utils/auth";
+// import { Data, Redirect } from './server/schemas/resolvers';
+function Quiz() {
+  const questions = [
+    {
+      questionText:
+        "What is your ideal climate?",
+      answerOptions: [
+        { answerText: "Tropical"},
+        { answerText: "Four seasons"},
+        { answerText: "70°F year round"},
+        { answerText: "Snow"},
+      ],
+    },
+    {
+      questionText: "What type of enviornment do you most want to live in?",
+      answerOptions: [
+        { answerText: "City"},
+        { answerText: "Beach"},
+        { answerText: "Mountains"},
+        { answerText: "Island"},
+      ],
+    },
+    {
+      questionText: "What are your language abilities?",
+      answerOptions: [
+        { answerText: "Speak more than 1 langugae" },
+        { answerText: "Only speak English"},
+        { answerText: "Learning new langauges" },
+      ],
+    },
+    {
+        questionText: "What is your favorite dish?",
+        answerOptions: [
+          { answerText: "Pizza" },
+          { answerText: "Sushi"},
+          { answerText: "Pasta" },
+          { answerText: "Salad" },
+        ],
+      },
+      {
+        questionText: "Which of these would you most likely spend your weekend doing?",
+        answerOptions: [
+          { answerText: "Coffee shops" },
+          { answerText: "Spa"},
+          { answerText: "going to a bar?"},
+          { answerText: "Movies"},
+        ],
+      },
+      {
+        questionText: "Which of th following are you most passionate about?",
+        answerOptions: [
+          { answerText: "Culture" },
+          { answerText: "Tradition"},
+          { answerText: "Adventure" },
+          { answerText: "Art" },
+        ],
+      },
+      {
+        questionText: "What's the perfect way to spend a day?",
+        answerOptions: [
+          { answerText: "Site seeing"},
+          { answerText: "Kayaking"},
+          { answerText: "Visiting a Winery" },
+          { answerText: "Shopping" },
+        ],
+      },
+      {
+        questionText: "Why do you like to travel?",
+        answerOptions: [
+          { answerText: "Escape responsibilities" },
+          { answerText: "See nature's beauties"},
+          { answerText: "Create memories with family/friends" },
+          { answerText: "Learn new cultures" },
+        ],
+      },
+    {
+      questionText: "Is having acess to high speed internet important to you?",
+      answerOptions: [
+        { answerText: "Yes"},
+        { answerText: "No"},
+      ],
+    },
+  ];
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+  const [score, setScore] = useState(0);
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowResult(true);
+    }
+  };
+
+  useEffect(() => {
+  if (setShowResult) {
+    setTimeout(() =>
+     3000);
+  }})
+
+  return (
+    <div className="app">
+      {showResult ? (
+         <Jumbotron>
+           <div className="score-section">
+          <h2>Thank you for completing the Questionaire.</h2>
+          <h2>You will be routed to a destination shortly.</h2>
+          <h2>Safe Travels!</h2>
+          <Redirect to="https://app-inter-globetrotters.herokuapp.com/products/62733a95e084c80022662316" />
+        </div>
+        </Jumbotron>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className="question-text">
+              {questions[currentQuestion].questionText}
+            </div>
+          </div>
+          <div className="answer-section">
+            {questions[currentQuestion].answerOptions.map((answerOption) => (
+              <button
+                onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}
+              >
+                {answerOption.answerText}
+              </button>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+export default Quiz;
